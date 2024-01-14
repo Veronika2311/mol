8cb0a1510807fea3597f45e8374edb450badd7e0
Veronika2311 <43279245+Veronika2311@users.noreply.github.com>
2024-01-15 00:39:35 +0300

    Add files via upload

---
+++ train_t5_augm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SBATCH --job-name=t5        # Название задачи
+SBATCH --error=t5-%j.err        # Файл для вывода ошибок
+SBATCH --output=t5-%j.log       # Файл для вывода результатов
+SBATCH --time=23:59:00         # Максимальное время выполнения
+SBATCH --ntasks=16             # Количество MPI процессов
+SBATCH --gpus=1                # Требуемое кол-во GPU
+SBATCH --cpus-per-task=2         # Требуемое кол-во CPU
+
+python transcript.py --df '/home/vaganeeva/chem_augm_finetune/data/augm_train_full.txt' \
+ --model_name '/home/vaganeeva/hugging_face/GT4SD/multitask-text-and-chemistry-t5-base-standard' \
+ --max_epochs 20 \
+ --batch_size 32
