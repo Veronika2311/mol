991b280d56af6b64eb7c63253e89f014efc1b146
Veronika2311 <Verona231100@yandex.ru>
2024-01-18 00:36:52 +0300

    Reduced batch size

--- sh/train_t5_augm.sh
+++ sh/train_t5_augm.sh
@@ -9,5 +9,5 @@
 python /home/vaganeeva/chem_augm_finetune/mol/code/trainscript.py --df '/home/vaganeeva/chem_augm_finetune/data/augm_train_full.txt' \
  --model_name '/home/vaganeeva/hugging_face/GT4SD/multitask-text-and-chemistry-t5-base-standard' \
  --max_epochs 20 \
- --batch_size 32 \
+ --batch_size 16 \
  --output_dir "/home/vaganeeva/chem_augm_finetune/trained_models/multitask-text-and-chemistry-t5-base-standard"
