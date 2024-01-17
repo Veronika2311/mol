2e89641f219de50abd632d6d723361f08ee311f8
Veronika2311 <Verona231100@yandex.ru>
2024-01-17 22:43:37 +0300

    Updated T5 training code and sh

--- code/trainscript.py
+++ code/trainscript.py
@@ -1,18 +1,19 @@
-import os
+import os.path
+
 import pandas as pd
-from sklearn.utils import shuffle
 from transformers import T5ForConditionalGeneration, AutoTokenizer
-import torch
-from torch.utils.data import Dataset, DataLoader
-from transformers import Trainer, TrainingArguments
-from transformers.file_utils import cached_property
-from typing import Tuple
-from sklearn.model_selection import train_test_split
-import gc
-from tqdm.auto import tqdm, trange
-from typing import List, Dict, Union
 import argparse
+import gc
 import logging
+from typing import List, Dict, Union
+
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+from tqdm.auto import tqdm
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+
 
 class PairsDataset(torch.utils.data.Dataset):
     def __init__(self, x, y):
@@ -25,14 +26,15 @@ class PairsDataset(torch.utils.data.Dataset):
         item['decoder_attention_mask'] = self.y['attention_mask'][idx]
         item['labels'] = self.y['input_ids'][idx]
         return item
-    
+
     @property
     def n(self):
         return len(self.x['input_ids'])
 
     def __len__(self):
-        return self.n # * 2
-    
+        return self.n  # * 2
+
+
 class DataCollatorWithPadding:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
@@ -45,56 +47,60 @@ class DataCollatorWithPadding:
         ybatch = self.tokenizer.pad(
             {'input_ids': batch['labels'], 'attention_mask': batch['decoder_attention_mask']},
             padding=True,
-        ) 
+        )
         batch['labels'] = ybatch['input_ids']
-        batch['decoder_attention_mask'] = ybatch['attention_mask']
-        
+        # TODO: note that I comment this
+        # batch['decoder_attention_mask'] = ybatch['attention_mask']
+
         return {k: torch.tensor(v) for k, v in batch.items()}
-    
+
+
 def cleanup():
     gc.collect()
     torch.cuda.empty_cache()
+
 
 def evaluate_model(model, test_dataloader):
     num = 0
     den = 0
     with torch.no_grad():
-        for batch in test_dataloader:
+        for batch in tqdm(test_dataloader):
             loss = model(**{k: v.to(model.device) for k, v in batch.items()}).loss
             num += len(batch) * loss.item()
             den += len(batch)
     val_loss = num / den
     return val_loss
 
+
 def train_loop(
-    model, train_dataloader, val_dataloader, 
-    max_epochs=30, 
-    max_steps=1_000, 
-    lr=3e-5,
-    gradient_accumulation_steps=1, 
-    cleanup_step=100,
-    report_step=300,
-    window=100,
+        model, train_dataloader, val_dataloader, output_dir,
+        max_epochs=30,
+        max_steps=1_000,
+        lr=3e-4,
+        gradient_accumulation_steps=1,
+        cleanup_step=100,
+        report_step=300,
+        window=100,
 ):
     cleanup()
-    optimizer = torch.optim.Adam(params = [p for p in model.parameters() if p.requires_grad], lr=lr)
+    optimizer = torch.optim.Adam(params=[p for p in model.parameters() if p.requires_grad], lr=lr)
 
-    #ewm_loss = 0
+    # ewm_loss = 0
     step = 0
-    
+
     model.train()
 
     for epoch in range(max_epochs):
         epoch_train_loss_sum = 0.
         num_epoch_steps = 0
-        print(f"Epoch {epoch} / {max_epochs}")
+        logging.info(f"Epoch {epoch} / {max_epochs}")
         if step >= max_steps:
             break
         tq = tqdm(train_dataloader)
         model.train()
         for i, batch in enumerate(tq):
             try:
-                batch['labels'][batch['labels']==0] = -100
+                batch['labels'][batch['labels'] == 0] = -100
                 loss = model(**{k: v.to(model.device) for k, v in batch.items()}).loss
                 epoch_train_loss_sum += float(loss)
                 num_epoch_steps += 1
@@ -114,12 +120,12 @@ def train_loop(
             if i % cleanup_step == 0:
                 cleanup()
 
-            #w = 1 / min(i+1, window)
-            #ewm_loss = ewm_loss * (1-w) + loss.item() * w
-            #tq.set_description(f'loss: {ewm_loss:4.4f}')
+            # w = 1 / min(i+1, window)
+            # ewm_loss = ewm_loss * (1-w) + loss.item() * w
+            # tq.set_description(f'loss: {ewm_loss:4.4f}')
             tq.set_description(f'loss: {loss:4.4f}')
 
-            #if (i and i % report_step == 0 or i == len(train_dataloader)-1)  and val_dataloader is not None:
+            # if (i and i % report_step == 0 or i == len(train_dataloader)-1)  and val_dataloader is not None:
             #    model.eval()
             #    eval_loss = evaluate_model(model, val_dataloader)
             #    model.train()
@@ -129,30 +135,37 @@ def train_loop(
         model.eval()
         train_loss = epoch_train_loss_sum / num_epoch_steps
         val_loss = evaluate_model(model, val_dataloader)
-        print(f'epoch {epoch}, step {i}/{step}. train loss: {train_loss:4.4f}  val loss: {val_loss:4.4f}')
-                    
-        #if step % 1000 == 0:
-            #model.save_pretrained(f't5_base_{dname}')
+        logging.info(f'epoch {epoch}. train loss: {train_loss:4.4f}  val loss: {val_loss:4.4f}')
+
+        # if step % 1000 == 0:
+        # model.save_pretrained(f't5_base_{dname}')
         if epoch % 1 == 0:
-            model.save_pretrained(f'/home/vaganeeva/chem_augm_finetune/trained_models/t5_base_{epoch}')
-        
+            checkpoint_dir = os.path.join(output_dir, f"e_{epoch}")
+            model.save_pretrained(checkpoint_dir)
+            # model.save_pretrained(f'/home/vaganeeva/chem_augm_finetune/trained_models/t5_base_{epoch}')
+
     cleanup()
 
 
-def train_model(x, y, model_name, test_size=0.1, batch_size=32, **kwargs):
+def train_model(x, y, model_name, max_length_molecule, max_length_text, test_size=0.1, batch_size=32, **kwargs):
     model = T5ForConditionalGeneration.from_pretrained(model_name).to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     x1, x2, y1, y2 = train_test_split(x, y, test_size=test_size, random_state=42)
-    train_dataset = PairsDataset(tokenizer(x1, padding='max_length', truncation=True), tokenizer(y1, padding='max_length', truncation=True))
-    test_dataset = PairsDataset(tokenizer(x2, padding='max_length', truncation=True), tokenizer(y2))
-    
+    train_dataset = PairsDataset(tokenizer(x1, padding='max_length', truncation=True, max_length=max_length_molecule),
+                                 tokenizer(y1, padding='max_length', truncation=True, max_length=max_length_text))
+    test_dataset = PairsDataset(tokenizer(x2, padding='max_length', truncation=True, max_length=max_length_molecule),
+                                tokenizer(y2))
+
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, drop_last=False, shuffle=True, collate_fn=data_collator)
-    val_dataloader = DataLoader(test_dataset, batch_size=batch_size, drop_last=False, shuffle=True, collate_fn=data_collator)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, drop_last=False, shuffle=True,
+                                  collate_fn=data_collator)
+    val_dataloader = DataLoader(test_dataset, batch_size=batch_size, drop_last=False, shuffle=True,
+                                collate_fn=data_collator)
 
     train_loop(model, train_dataloader, val_dataloader, **kwargs)
     return model
+
 
 def parse_args():
     """
@@ -160,51 +173,67 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='t5 train')
 
-    # parser.add_argument('--df', type=str, required=True,
-    #                     help='train df', )
-    # parser.add_argument('--model_name', type=str, required=True,
-    #                     help='model name',)
-    # parser.add_argument('--max_epochs', type=int, required=True,
-    #                     help='max_epochs',)
-    # parser.add_argument('--batch_size', type=int, required=True,
-    #                     help='batch_size', )
+    parser.add_argument('--df', type=str, required=True,
+                        help='train df', )
+    parser.add_argument('--model_name', type=str, required=True,
+                        help='model name', )
+    parser.add_argument('--max_epochs', type=int, required=True,
+                        help='max_epochs', )
+    parser.add_argument('--max_length_molecule', type=int, default=512, required=False)
+    parser.add_argument('--max_length_text', type=int, default=512, required=False)
+    parser.add_argument('--batch_size', type=int, required=True,
+                        help='batch_size', )
+    parser.add_argument('--output_dir', required=True)
 
-    parser.add_argument('--df', type=str, required=False,
-                        help='train df', default='C:/Users/veron/notebooks/biochemnlp/augm_train_full.txt')
-    parser.add_argument('--model_name', type=str, required=False,
-                        help='model name', default='C:/Users/veron/notebooks/biochemnlp/model')
-    parser.add_argument('--max_epochs', type=int, required=False,
-                        help='max_epochs', default=3)
-    parser.add_argument('--batch_size', type=int, required=False,
-                        help='batch_size', default=2)    
+    # parser.add_argument('--df', type=str, required=False,
+    #                     help='train df', default='C:/Users/veron/notebooks/biochemnlp/augm_train_full.txt')
+    # parser.add_argument('--model_name', type=str, required=False,
+    #                     help='model name', default='C:/Users/veron/notebooks/biochemnlp/model')
+    # parser.add_argument('--max_epochs', type=int, required=False,
+    #                     help='max_epochs', default=3)
+    # parser.add_argument('--max_length_molecule', type=int, default=512)
+    # parser.add_argument('--max_length_text', type=int, default=512)
+    # parser.add_argument('--batch_size', type=int, required=False,
+    #                     help='batch_size', default=2)
+    #
+    # parser.add_argument('--output_dir', default="delete/", required=False)
 
     args = parser.parse_args()
     return args
 
 
 def main(args):
+    logging.info("Training with parameters:")
+    for k, v in vars(args).items():
+        logging.info(f"{k} : {v}")
     cleanup()
-    #device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    output_dir = args.output_dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    max_length_molecule = args.max_length_molecule
+    max_length_text = args.max_length_text
+    # device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-    #model_name = 'C:/Users/veron/notebooks/biochemnlp/model'
-    #df = pd.read_csv('C:/Users/veron/notebooks/biochemnlp/augm_train_full.txt', sep='\t')
+    # model_name = 'C:/Users/veron/notebooks/biochemnlp/model'
+    # df = pd.read_csv('C:/Users/veron/notebooks/biochemnlp/augm_train_full.txt', sep='\t')
 
     datasets = {
         'train': pd.read_csv(args.df, sep='\t')
-        }
+    }
     steps = 1_000_000
     for dname, d in datasets.items():
         print(f'\n\n\n  {dname}\n=====================\n\n')
-        model = train_model(d['SMILES'].tolist(), d['description'].tolist(), model_name=args.model_name, 
-                            batch_size=args.batch_size, max_epochs=args.max_epochs, max_steps=steps)
-        model.save_pretrained(f'/home/vaganeeva/chem_augm_finetune/trained_models/t5_base_{dname}')
+        model = train_model(d['SMILES'].tolist(), d['description'].tolist(), model_name=args.model_name,
+                            batch_size=args.batch_size, max_epochs=args.max_epochs, max_steps=steps,
+                            max_length_text=max_length_text, max_length_molecule=max_length_molecule,
+                            output_dir=output_dir)
+        model.save_pretrained(os.path.join(output_dir, "final_checkpoint/"))
 
 
 if __name__ == '__main__':
-
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     args = parse_args()
-    
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S', )
     main(args)
--- sh/train_t5_augm.sh
+++ sh/train_t5_augm.sh
@@ -2,11 +2,12 @@
 #SBATCH --job-name=t5        # Название задачи
 #SBATCH --error=/home/vaganeeva/chem_augm_finetune/logs/train_augm_chem_t5.err        # Файл для вывода ошибок
 #SBATCH --output=/home/vaganeeva/chem_augm_finetune/logs/train_augm_chem_t5.log       # Файл для вывода результатов
-#SBATCH --time=23:59:00         # Максимальное время выполнения
+#SBATCH --time=32:59:00         # Максимальное время выполнения
 #SBATCH --gpus=1                # Требуемое кол-во GPU
-#SBATCH --cpus-per-task=1         # Требуемое кол-во CPU
+#SBATCH --cpus-per-task=2         # Требуемое кол-во CPU
 
 python /home/vaganeeva/chem_augm_finetune/mol/code/trainscript.py --df '/home/vaganeeva/chem_augm_finetune/data/augm_train_full.txt' \
  --model_name '/home/vaganeeva/hugging_face/GT4SD/multitask-text-and-chemistry-t5-base-standard' \
  --max_epochs 20 \
- --batch_size 16
+ --batch_size 32 \
+ --output_dir "/home/vaganeeva/chem_augm_finetune/trained_models/multitask-text-and-chemistry-t5-base-standard"
